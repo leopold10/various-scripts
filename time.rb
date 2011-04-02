c49ad9388f067c88ce88e6e34107df5c75baae32
Leopold <leopold@akoni.eu>
2011-04-03 03:48:13 +0900

    keep it early

--- time.rb
+++ time.rb
@@ -1,7 +1,7 @@
 
 require 'rubygems'
 
-puts 'Good evening, Peter'
+puts 'Good morning, Peter'
 puts 'Do you have a watch?'
 puts 'If not then I can tell you what time it is:'
 puts 'It is now ' + Time.now.to_s
